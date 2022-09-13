8ec6ef138f6457bce9d34e198c0d57a75c3938ef
Tania1983 <113310602+Tania1983@users.noreply.github.com>
2022-09-14 00:54:34 +0200

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ export class AppComponent {
   nombre: string = 'Tanya';
   edad: Number = 39;
   miLogo: string =
-    'https://kesizo.github.io/assets/images/kesizo-logo-6-832x834.png';
+    'https://previews.123rf.com/images/majivecka/majivecka1605/majivecka160500071/56574897-ilustraci%C3%B3n-del-vector-del-logotipo-del-gato-sobre-un-fondo-blanco-.jpg';
 }
